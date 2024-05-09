94238de380242746da4d7945bcfb597e823d1232
SnowzNZ <98007575+SnowzNZ@users.noreply.github.com>
2024-05-09 19:05:48 +1200

    init

---
+++ src/main/java/dev/snowz/infinitystonesprotection/InfinityStonesProtection.java
@@ -0,0 +1,73 @@
+package dev.snowz.infinitystonesprotection;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Item;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class InfinityStonesProtection extends JavaPlugin implements Listener {
+
+    private final Map<String, Material> infinityStones = new HashMap<String, Material>() {{
+        put("§eMind Stone", Material.YELLOW_DYE);
+        put("§cReality Stone", Material.RED_DYE);
+        put("§6Soul Stone", Material.ORANGE_DYE);
+        put("§aTime Stone", Material.LIME_DYE);
+        put("§9Space Stone", Material.BLUE_DYE);
+        put("§5Power Stone", Material.PURPLE_DYE);
+    }};
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onCraftItem(CraftItemEvent e) {
+        HumanEntity player = e.getWhoClicked();
+        ItemStack craftedItem = e.getCurrentItem();
+
+        if (craftedItem != null &&
+                craftedItem.hasItemMeta() &&
+                Objects.requireNonNull(craftedItem.getItemMeta()).hasDisplayName() &&
+                infinityStones.containsKey(craftedItem.getItemMeta().getDisplayName()) &&
+                craftedItem.getType() == infinityStones.get(craftedItem.getItemMeta().getDisplayName())) {
+
+            e.setCancelled(true);
+            e.getInventory().clear();
+            player.closeInventory();
+
+            World world = player.getWorld();
+            Location location = player.getLocation();
+
+            Item droppedItem = world.dropItem(location, craftedItem);
+
+            droppedItem.setPickupDelay(20 * getConfig().getInt("durationInSeconds"));
+            droppedItem.setInvulnerable(true);
+
+            if (getConfig().getBoolean("glowing")) {
+                PotionEffect glowingEffect = new PotionEffect(PotionEffectType.GLOWING, 20 * getConfig().getInt("durationInSeconds"), 1);
+                player.addPotionEffect(glowingEffect);
+            }
+
+            if (getConfig().getBoolean("lightning")) {
+                world.strikeLightningEffect(location);
+            }
+
+            getServer().broadcastMessage("§c§l" + player.getName() + "§r has crafted a " + craftedItem.getItemMeta().getDisplayName() + "§r at §b" + Math.round(location.getX()) + ", " + Math.round(location.getY()) + ", " + Math.round(location.getZ()) + "§r.");
+        }
+
+    }
+}
