bf243679e51d77cf3892622b2acf203484167978
SnowzNZ <98007575+SnowzNZ@users.noreply.github.com>
2024-05-11 23:05:44 +1200

    prevent item going into hoppers during duration

--- src/main/java/dev/snowz/infinitystonesprotection/InfinityStonesProtection.java
+++ src/main/java/dev/snowz/infinitystonesprotection/InfinityStonesProtection.java
@@ -3,16 +3,20 @@ package dev.snowz.infinitystonesprotection;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Hopper;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.InventoryPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -27,6 +31,9 @@ public class InfinityStonesProtection extends JavaPlugin implements Listener {
         put("§9Space Stone", Material.BLUE_DYE);
         put("§5Power Stone", Material.PURPLE_DYE);
     }};
+
+    private final ArrayList<Item> droppedItems = new ArrayList<>();
+    private final PotionEffect glowingEffect = new PotionEffect(PotionEffectType.GLOWING, 20 * getConfig().getInt("durationInSeconds"), 1);
 
     @Override
     public void onEnable() {
@@ -53,12 +60,19 @@ public class InfinityStonesProtection extends JavaPlugin implements Listener {
             Location location = player.getLocation();
 
             Item droppedItem = world.dropItem(location, craftedItem);
-
             droppedItem.setPickupDelay(20 * getConfig().getInt("durationInSeconds"));
             droppedItem.setInvulnerable(true);
 
+            droppedItems.add(droppedItem);
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    droppedItems.remove(droppedItem);
+                }
+            }.runTaskLater(this, 20L * getConfig().getInt("durationInSeconds"));
+            System.out.println(droppedItems);
+
             if (getConfig().getBoolean("glowing")) {
-                PotionEffect glowingEffect = new PotionEffect(PotionEffectType.GLOWING, 20 * getConfig().getInt("durationInSeconds"), 1);
                 player.addPotionEffect(glowingEffect);
             }
 
@@ -69,5 +83,12 @@ public class InfinityStonesProtection extends JavaPlugin implements Listener {
             getServer().broadcastMessage("§c§l" + player.getName() + "§r has crafted a " + craftedItem.getItemMeta().getDisplayName() + "§r at §b" + Math.round(location.getX()) + ", " + Math.round(location.getY()) + ", " + Math.round(location.getZ()) + "§r.");
         }
 
+    }
+
+    @EventHandler()
+    public void onInventoryPickupItem(InventoryPickupItemEvent e) {
+        if (e.getInventory().getHolder() instanceof Hopper && droppedItems.contains(e.getItem())) {
+            e.setCancelled(true);
+        }
     }
 }
